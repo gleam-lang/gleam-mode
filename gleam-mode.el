4b4d424dedd8ac76860281d0b539f448f2210929
Jonathan Arnett <jonarnett90@gmail.com>
2022-01-04 12:46:07 -0500

    Initial commit

--- gleam-mode.el
+++ gleam-mode.el
@@ -1,34 +1,133 @@
-;;; gleam-mode.el --- emacs major mode for the Gleam language
+;;; gleam-mode.el --- Major mode for Gleam -*- lexical-binding: t -*-
 
-(setq gleam-font-lock-keywords
-      (let* (
-            ;; define several category of keywords
-            (x-keywords '("as" "assert" "case" "const" "external" "fn" "if"
-                          "import" "let" "opaque" "pub" "todo" "try" "type"))
-            (x-booleans '("True" "False"))
+;; Copyright © 2021 Louis Pilfold <louis@lpil.uk>
+;; Authors: Jonathan Arnett <jonathan.arnett@protonmail.com>
+;;
+;; URL: https://github.com/gleam-lang/gleam-mode
+;; Keywords: languages gleam
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "25.1") (tree-sitter "0.15.0"))
 
-            ;; generate regex string for each category of keywords
-            (x-keywords-regexp (regexp-opt x-keywords 'symbols))
-            (x-booleans-regexp (regexp-opt x-booleans 'words)))
+;; This file is NOT part of GNU Emacs.
 
-        `(
-          (,x-keywords-regexp . font-lock-keyword-face)
-          (,x-booleans-regexp . font-lock-constant-face)
-          ("fn \\([_[:alpha:]]+\\)(" 1 font-lock-function-name-face)
-          ("[[:upper:]][[:alpha:]]*" . font-lock-type-face)
-          ;; note: order above matters, because once colored, that part won't change.
-          ;; in general, put longer words first
-          )))
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this package. If not, see https://www.gnu.org/licenses.
+
+;;; Commentary:
+
+;; Provides syntax highlighting, indentation, and code navigation
+;; features for the Gleam programming language.
+
+;;; Code:
+
+(require 'tree-sitter)
+(require 'tree-sitter-indent)
+
+
+;;; Customization
+(defgroup gleam nil
+  "Major mode for Gleam"
+  :prefix "gleam-"
+  :group 'languages)
+
+(defcustom gleam-mode-hook nil
+  "Hook that runs when gleam-mode starts."
+  :type 'hook)
+
+(defvar gleam-indent-offset 2
+  "Offset used to indent Gleam code.")
+
+
+;;; Mode definitions
+
+(defvar gleam-mode-map
+  (let ((map (make-sparse-keymap)))
+    map)
+  "Keymap used in gleam-mode buffers.")
+
+(defcustom tree-sitter-indent-gleam-scopes
+  '((indent-all . ())
+    (indent-rest . (
+                    target_group
+                    import
+                    function
+                    public_function
+                    anonymous_function
+                    external_function
+                    public_external_function
+                    type_definition
+                    public_type_definition
+                    public_opaque_type_definition
+                    type_alias
+                    public_type_alias
+                    arguments
+                    parameters
+                    case
+                    case_clause))
+    (indent-body . ())
+    (paren-indent . ())
+    (multi-line-text . ())
+    (outdent . ("}" ")")))
+  "`tree-sitter-indent' indentation rules based on AST.")
 
 ;;;###autoload
-(define-derived-mode gleam-mode c-mode "gleam mode"
-  "Major mode for editing gleam"
-  ;; use // for commenting lines
-  (setq-local comment-start "// ")
-  (setq-local comment-end "")
+(add-to-list 'auto-mode-alist '("\\.gleam" . gleam-mode))
 
-  ;; code for syntax highlighting
-  (setq font-lock-defaults '((gleam-font-lock-keywords))))
+;;;###autoload
+(define-derived-mode gleam-mode prog-mode "Gleam"
+  "Major mode for Gleam.
 
-;; add the mode to the `features' list
+Key bindings:
+\\{gleam-mode-map}"
+
+  ;; Configure tree-sitter and friends
+  (add-to-list 'tree-sitter-load-path gleam-mode--tree-sitter-dir)
+  (tree-sitter-load 'gleam)
+  (add-to-list 'tree-sitter-major-mode-language-alist '(gleam-mode . gleam))
+  (setq tree-sitter-hl-default-patterns (gleam-mode--read-highlight-query))
+
+  (tree-sitter-mode)
+  (tree-sitter-hl-mode)
+
+  (setq-local indent-tabs-mode nil)
+  (tree-sitter-indent-mode))
+
+
+;;; Private functions
+
+(defconst gleam-mode--dir
+  (file-name-directory (locate-library "gleam-mode.el"))
+  "The directory where the library `gleam-mode' is located.")
+
+(defconst gleam-mode--tree-sitter-dir
+  (concat gleam-mode--dir
+          (file-name-as-directory "tree-sitter-gleam"))
+  "The directory where `tree-sitter-gleam' is located.")
+
+(defconst gleam-mode--queries-dir
+  (concat gleam-mode--tree-sitter-dir
+          (file-name-as-directory "queries"))
+  "The directory where the tree-sitter-gleam query files can be found.")
+
+(defconst gleam-mode--highlights-query-file
+  (concat gleam-mode--queries-dir "highlights.scm")
+  "The file containing the highlight queries for tree-sitter-gleam.")
+
+(defun gleam-mode--read-highlight-query ()
+  "Read the contents of the tree-sitter-gleam highlight query."
+  (with-temp-buffer
+    (insert-file-contents gleam-mode--highlights-query-file)
+    (buffer-string)))
+
 (provide 'gleam-mode)
+;;; gleam-mode.el ends here
