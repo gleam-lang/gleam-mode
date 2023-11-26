d6bf6651c5f77bb9ed89e9f802898693541a28df
Jonathan Arnett <jonarnett90@gmail.com>
2023-11-26 11:25:51 -0500

    First pass at gleam-ts-mode

--- README.md
+++ README.md
@@ -1,19 +1,17 @@
 An Emacs Major Mode for [Gleam]
 ===============================
 
-This mode uses [tree-sitter] under-the-hood for syntax highlighting and code
-navigation.
+This mode uses [tree-sitter] (and Emacs 29's `treesit` package) under-the-hood for syntax highlighting and code navigation.  If you're using an Emacs that's older than 29 (run `M-x version` to find out), or a version compiled without `treesit` (run `M-: (treesit-available-p)` to find out), you probably want [gleam-mode] instead.
 
 https://user-images.githubusercontent.com/2058614/151681785-5d212e1b-191b-4e7d-a868-f6e42d7efe61.mp4
 
 Setup
 -----
 
-This project will eventually be hosted on [MELPA]. However, until that time, the
-best way to install this is first to clone the project:
+This project will eventually be hosted on [MELPA]. However, until that time, the best way to install this is first to clone the project:
 
 ```
-$ git clone --recurse-submodules git@github.com:gleam-lang/gleam-mode
+$ git clone git@github.com:gleam-lang/gleam-ts-mode
 ```
 
 Then you'll need to load this from your init script (`~/.emacs` or
@@ -22,69 +20,37 @@ Then you'll need to load this from your init script (`~/.emacs` or
 ### use-package (recommended)
 
 ```elisp
-(use-package gleam-mode
-  :load-path "~/path/to/gleam-mode")
+(use-package gleam-ts-mode
+  :load-path "~/path/to/gleam-ts-mode")
 ```
 
-Replace `~/path/to/gleam-mode` with the path where you cloned gleam-mode.
+Replace `~/path/to/gleam-ts-mode` with the path where you cloned gleam-mode.
 
 ### vanilla
 
 ```elisp
-(add-to-list 'load-path "~/path/to/gleam-mode")
-(load-library "gleam-mode")
+(add-to-list 'load-path "~/path/to/gleam-ts-mode")
+(load-library "gleam-ts-mode")
 ```
 
-Replace `~/path/to/gleam-mode` with the path where you cloned gleam-mode.
-
-Configuration
--------------
-
-gleam-mode ships with a `gleam-format` command that is not bound by default to
-any keybinding. To bind it:
-
-### use-package
-
-Add `:bind (:map gleam-mode-map (<binding> . gleam-format))` to your use-package declaration. e.g.
-
-```elisp
-(use-package gleam-mode
-  :load-path "~/path/to/gleam-mode"
-  :bind (:map gleam-mode-map
-              ("C-c g f" . gleam-format)))
-```
-
-(here "C-c g f" means `Control`+`C` followed by `g` followed by `f`)
-
-### vanilla
-
-Add the following after the lines where you setup gleam-mode:
-
-```elisp
-(define-key gleam-mode-map (kbd "C-c g f") 'gleam-format)
-```
-
-(here "C-c g f" means `Control`+`C` followed by `g` followed by `f`)
+Replace `~/path/to/gleam-ts-mode` with the path where you cloned gleam-mode.
 
 TODO
 ----
 
 - [x] Syntax highlighting
-- [x] Indentation (at least somewhat)
+- [ ] Indentation
+- [ ] Imenu integration
 - [x] Formatting
 - [ ] Completion?
 - [ ] REPL?
 
 [Gleam]: https://gleam.run
 [tree-sitter]: https://github.com/tree-sitter/tree-sitter
+[gleam-mode]: https://github.com/gleam-lang/gleam-mode
 [MELPA]: https://melpa.org
 
 License
 -------
 
-This program is licensed under The Apache License, Version 2.0 or, at your
-option, under the terms of the GNU General Public License as published by the
-Free Software Foundation; either version 3, or (at your option) any later
-version. See [LICENSE-apache](./LICENSE-apache) for the terms of the Apache
-License, Version 2.0 or [LICENSE-gpl](./LICENSE-gpl) for the terms of the GNU
-Public License, Version 3.
+This program is licensed under The Apache License, Version 2.0 or, at your option, under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3, or (at your option) any later version. See [LICENSE-apache](./LICENSE-apache) for the terms of the Apache License, Version 2.0 or [LICENSE-gpl](./LICENSE-gpl) for the terms of the GNU Public License, Version 3.
---
+++ gleam-ts-mode.el
@@ -0,0 +1,271 @@
+;;; gleam-ts-mode.el --- Major mode for Gleam -*- lexical-binding: t -*-
+
+;; Copyright © 2023 Louis Pilfold <louis@lpil.uk>
+;; Authors: Jonathan Arnett <jonathan.arnett@protonmail.com>
+;;
+;; URL: https://github.com/gleam-lang/gleam-ts-mode
+;; Keywords: languages gleam
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "29.1"))
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is licensed under The Apache License¹, Version 2.0 or,
+;; at your option, under the terms of the GNU General Public License²
+;; as published by the Free Software Foundation; either version 3, or
+;; (at your option) any later version.
+
+;; ¹ You may not use this file except in compliance with the License.
+;; You may obtain a copy of the License at
+;;
+;; http://www.apache.org/licenses/LICENSE-2.0
+;;
+;; Unless required by applicable law or agreed to in writing, software
+;; distributed under the License is distributed on an "AS IS" BASIS,
+;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;; See the License for the specific language governing permissions and
+;; limitations under the License.
+
+;; ² This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this package. If not, see https://www.gnu.org/licenses.
+
+;;; Commentary:
+
+;; Provides syntax highlighting, indentation, and code navigation
+;; features for the Gleam programming language.
+
+;;; Code:
+
+(require 'prog-mode)
+(require 'treesit)
+
+
+;;; Customization
+
+(defgroup gleam-ts nil
+  "Major mode for editing Gleam."
+  :prefix "gleam-ts-"
+  :group 'languages)
+
+(defcustom gleam-ts-indent-offset 2
+  "Offset used to indent Gleam code."
+  :type 'integer
+  :safe 'integerp
+  :group 'gleam-ts)
+
+
+;;; Tree-sitter font locking
+
+(defface gleam-ts-constructor-face
+  '((t (:inherit font-lock-type-face)))
+  "Font used for highlighting Gleam type constructors.")
+
+(defface gleam-ts-module-face
+  '((t (:inherit font-lock-variable-name-face)))
+  "Font used for highlighting Gleam modules.")
+
+(defvar gleam-ts--font-lock-settings
+  (treesit-font-lock-rules
+   :feature 'comment
+   :language 'gleam
+   '((comment) @font-lock-comment-face)
+
+   :feature 'string
+   :language 'gleam
+   '((string) @font-lock-string-face)
+
+   :feature 'number
+   :language 'gleam
+   '((integer) @font-lock-number-face
+     (float) @font-lock-number-face)
+
+   :feature 'function-name
+   :language 'gleam
+   '((unqualified_import (identifier) @font-lock-function-name-face)
+     (function
+      name: (identifier) @font-lock-function-name-face)
+     (external_function
+      name: (identifier) @font-lock-function-name-face)
+     (function_call
+      function: (identifier) @font-lock-function-name-face))
+
+   :feature 'variable-name
+   :language 'gleam
+   '((identifier) @font-lock-variable-name-face)
+
+   :feature 'constructor
+   :language 'gleam
+   '((unqualified_import (type_identifier) @gleam-ts-constructor-face)
+     (constructor_name) @gleam-ts-constructor-face)
+
+   :feature 'type-name
+   :language 'gleam
+   '((unqualified_import "type" (type_identifier) @font-lock-type-face)
+     (remote_type_identifier) @font-lock-type-face
+     (type_identifier) @font-lock-type-face)
+
+   :feature 'constant-name
+   :language 'gleam
+   :override t
+   '((constant
+      name: (identifier) @font-lock-constant-face))
+
+   :feature 'keyword
+   :language 'gleam
+   '([
+      (visibility_modifier)
+      (opacity_modifier)
+      "as"
+      "assert"
+      "case"
+      "const"
+      ;; Deprecated
+      "external"
+      "fn"
+      "if"
+      "import"
+      "let"
+      "panic"
+      "todo"
+      "type"
+      "use"
+      ] @font-lock-keyword-face)
+
+   :feature 'operator
+   :language 'gleam
+   '((binary_expression operator: _ @font-lock-operator-face)
+     (boolean_negation "!" @operator)
+     (integer_negation "-" @operator))
+
+   :feature 'property
+   :language 'gleam
+   '((label) @font-lock-property-name-face
+     (tuple_access
+      index: (integer) @font-lock-property-name-face))
+
+   :feature 'annotation
+   :language 'gleam
+   :override t
+   '((attribute
+      "@" @font-lock-preprocessor-face
+      name: (identifier) @font-lock-preprocessor-face))
+
+   :feature 'documentation
+   :language 'gleam
+   '((module_comment) @font-lock-doc-face
+     (statement_comment) @font-lock-doc-face)
+
+   :feature 'module
+   :language 'gleam
+   :override t
+   '((module) @gleam-ts-module-face
+     (import alias: (identifier) @gleam-ts-module-face)
+     (remote_type_identifier
+      module: (identifier) @gleam-ts-module-face)
+     (remote_constructor_name
+      module: (identifier) @gleam-ts-module-face)
+     ;; Unfortunately #is-not? local doesn't work here
+     ;; ((field_access
+     ;;   record: (identifier) @gleam-ts-module-face)
+     ;;  (#is-not? local))
+     )
+
+   :feature 'builtin
+   :language 'gleam
+   :override t
+   '((bit_string_segment_option) @font-lock-builtin-face)
+
+   :feature 'bracket
+   :language 'gleam
+   '([
+      "("
+      ")"
+      "["
+      "]"
+      "{"
+      "}"
+      "<<"
+      ">>"
+      ] @font-lock-bracket-face)
+
+   :feature 'delimiter
+   :language 'gleam
+   '([
+      "."
+      ","
+      ;; Controversial -- maybe some are operators?
+      ":"
+      "#"
+      "="
+      "->"
+      ".."
+      "-"
+      "<-"
+      ] @font-lock-delimiter-face)))
+
+
+;;; Public functions
+
+(defun gleam-ts-install-grammar ()
+  "Install the Gleam tree-sitter grammar."
+  (interactive)
+  (if (and (treesit-available-p) (boundp 'treesit-language-source-alist))
+      (let ((treesit-language-source-alist
+             (cons
+              '(gleam . ("https://github.com/gleam-lang/tree-sitter-gleam"))
+              treesit-language-source-alist)))
+        (treesit-install-language-grammar 'gleam))
+    (display-warning 'treesit "Emacs' treesit package does not appear to be available")))
+
+(defun gleam-ts-format ()
+  "Format the current buffer using the `gleam format' command."
+  (interactive)
+  (if (executable-find "gleam")
+      (save-restriction ; Save the user's narrowing, if any
+        (widen)         ; Expand scope to the whole, unnarrowed buffer
+        (let* ((buf (current-buffer))
+               (min (point-min))
+               (max (point-max))
+               (tmpfile (make-nearby-temp-file "gleam-format")))
+          (unwind-protect
+              (with-temp-buffer
+                (insert-buffer-substring-no-properties buf min max)
+                (write-file tmpfile)
+                (call-process "gleam" nil nil nil "format" (buffer-file-name))
+                (revert-buffer :ignore-autosave :noconfirm)
+                (let ((tmpbuf (current-buffer)))
+                  (with-current-buffer buf
+                    (replace-buffer-contents tmpbuf))))
+            (if (file-exists-p tmpfile) (delete-file tmpfile)))
+          (message "Formatted!")))
+    (display-warning 'gleam-ts "`gleam' executable not found!")))
+
+
+;;; Major mode definition
+
+(define-derived-mode gleam-ts-mode prog-mode "Gleam"
+  "Major mode for editing Gleam.
+
+\\<gleam-ts-mode-map>"
+  :group 'gleam-ts
+
+  (cond
+   ((treesit-ready-p 'gleam)
+    (treesit-parser-create 'gleam)
+
+    (setq-local treesit-font-lock-settings gleam-ts--font-lock-settings)
+    (setq-local treesit-font-lock-feature-list
+                '((comment string number function-name variable-name constructor type-name)
+                  (constant-name keyword operator property)
+                  (annotation documentation module builtin bracket delimiter)))
+    (treesit-major-mode-setup))
+   (t
+    (message "Cannot load Gleam tree-sitter-gleam."))))
+
+(provide 'gleam-ts-mode)
+;;; gleam-ts-mode.el ends here
