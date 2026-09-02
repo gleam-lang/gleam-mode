5fd0ad952e60ee0032688cb7c2d9fd09e79eff62
Felix Dumbeck <felix@dumbeck.net>
2026-09-02 13:58:54 +0200

    Fix indentation rules for bit arrays, and add rules for data constructors (#66)
    The bit array indentation rules were broken because of this missing comma:
    
    ```lisp
    ((parent-is ,gleam-ts--bit-array-regex) parent-bol ,offset)
    ;;          ^ this was missing
    ```
    
    This change also adds new indentation rules for `data_constructor`, `data_constructors`, and `data_constructor_arguments`.

--- gleam-ts-mode.el
+++ gleam-ts-mode.el
@@ -309,8 +309,11 @@ A change to this setting only takes effect the next time the
        ((parent-is "^list$") parent-bol ,offset)
        ((parent-is "^let$") parent-bol ,offset)
        ((parent-is "^let_assert$") parent-bol ,offset)
+       ((parent-is "^data_constructors$") parent-bol 0)
+       ((parent-is "^data_constructor$") parent-bol ,offset)
+       ((parent-is "^data_constructor_arguments$") parent-bol ,offset)
        ;; HACK: This makes both v1.0.0 and mainline tree-sitter-gleam happy.
-       ((parent-is gleam-ts--bit-array-regex) parent-bol ,offset)))))
+       ((parent-is ,gleam-ts--bit-array-regex) parent-bol ,offset)))))
 
 (defun gleam-ts--grand-parent-bol (_n parent &rest _)
   "Return the beginning of line for the PARENT's parent's parent."
